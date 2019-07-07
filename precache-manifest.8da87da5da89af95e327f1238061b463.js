dc9219dd80ee2e74d7ca3b0c6888cb43c51bb5e1
Wesley A <33585141+ayelsew@users.noreply.github.com>
2019-07-06 22:54:21 -0300

    first commit

---
+++ precache-manifest.8da87da5da89af95e327f1238061b463.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44bffbba25aa2c84dc3a9c5564e501f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4adb94a97c4e81e80cb",
+    "url": "/static/css/main.e3653cc6.chunk.css"
+  },
+  {
+    "revision": "3f8ab98683934d718ec6",
+    "url": "/static/js/2.bf4eefd7.chunk.js"
+  },
+  {
+    "revision": "e4adb94a97c4e81e80cb",
+    "url": "/static/js/main.8a185bb1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
